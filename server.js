c79cf396be30944e0d4344135e3b80fb8f2a32d2
Ilsur <yuzlikeyevi@gmail.com>
2022-09-06 19:10:26 -0500

    Index working, product schema created

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const mongoose = require('mongoose');
+const Product = require('./models/products');
 
 // ** Databse Connection **
 mongoose.connect(process.env.DATABASE_URL, {
@@ -20,3 +21,31 @@ db.on('disconnected', () => console.log('mongo disconnected'));
 // ** Listener **
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`server is listning on port: ${PORT}`));
+
+// ** Middleware **
+// ** Body parser middleware: give us access to req.body **
+app.use(express.urlencoded({ extended: true }));
+
+// ** Index **
+app.get('/store', (req, res) => {
+    Product.find({}, (error, allProducts) => {
+        res.render('index.ejs', {
+            products: allProducts,
+        });
+    });
+});
+// ** ROUTES **
+
+// ** SEED **
+
+// ** NEW **
+
+// ** DELETE **
+
+// ** UPDATE **
+
+// ** CREATE **
+
+// ** EDIT **
+
+// ** SHOW **
